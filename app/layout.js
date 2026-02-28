adc085d2f473d0ca5e8812ab0d48446407761ab6
hazemkego-spec <hazemkego@gmail.com>
2026-02-28 08:49:04 +0200

    Update and rename layout.tsx to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,16 @@
-import React from "react";
-
 export const metadata = {
   title: "Mini Talabat | ميني طلبات",
   description: "أول مول تجاري رقمي في جيبك",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
         <link rel="icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo.png" />
       </head>
-      <body style={{ margin: 0 }}>
-        {children}
-      </body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
