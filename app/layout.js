9389579409942044c337ced4bf3c4d9b5cc238b1
hazemkego-spec <hazemkego@gmail.com>
2026-02-28 16:57:16 +0200

    Add viewport meta tag for responsive design

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body style={{ margin: 0 }}>
         {children}
