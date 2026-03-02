9a0759da60dadf32727679717849409ba4a71097
hazemkego-spec <hazemkego@gmail.com>
2026-03-02 06:14:44 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export const metadata = {
   title: 'Mini Talabat | ميني طلبات',
   description: 'أكبر مول تجاري رقمي في جيبك',
-  manifest: '/manifest.json', 
+  manifest: '/manifest.json',
   icons: {
     icon: '/mall-logo.png',
     apple: '/mall-logo.png',
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/mall-logo.png" />
         <link rel="apple-touch-icon" href="/mall-logo.png" />
         <meta name="theme-color" content="#FF6600" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </head>
       <body>{children}</body>
     </html>
