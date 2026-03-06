39f4d39682bb50ace9d402e588df2fe00dccd7ab
hazemkego-spec <hazemkego@gmail.com>
2026-03-07 01:17:55 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -166,7 +166,7 @@ export default function MiniTalabat() {
             {/* صورة الكفر الخلفية - أطول وأوضح */}
             <div style={{ 
               width: '100%', 
-              height: '220px', 
+              height: '210px', 
               backgroundImage: 'url("/cover.png")', 
               backgroundSize: 'cover', 
               backgroundPosition: 'center', 
