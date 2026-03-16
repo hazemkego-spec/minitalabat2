3d8e29012b9cc6cd88c89a67ef6926b9f17a7dac
hazemkego-spec <hazemkego@gmail.com>
2026-03-16 07:52:53 +0200

    Update NavBar.js

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -94,7 +94,7 @@ export default function NavBar({ activeTab, setActiveTab, setSelectedShop, hasSe
         }}
       >
         <span>🏪</span>
-        <span>المتجر</span>
+        <span>تعليمات و تواصل معنا</span>
       </button>
     </nav>
   );
