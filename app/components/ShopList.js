b820df59a1c513d98f55a9a303787426e0c42c2d
hazemkego-spec <hazemkego@gmail.com>
2026-03-18 05:33:45 +0200

    Update ShopList.js

--- app/components/ShopList.js
+++ app/components/ShopList.js
@@ -224,7 +224,7 @@ cover: "/sawan-logo.png",
   {
     id: 5, // تأكد من أن الـ ID غير مكرر
     category: "صيدليات",
-whatsapp: "201158683236"
+whatsapp: "201158683236",
     name: "صيدلية د/ هاني فاروق",
     isOpen: true,
     logo: "/HanyFarPharmlogo.png",
