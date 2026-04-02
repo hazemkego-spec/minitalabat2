8a398e85d30a79837cb29ddcf009121d3f43ad3a
hazemkego-spec <hazemkego@gmail.com>
2026-04-02 08:57:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,33 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // ✅ إضافة إعدادات الرأس (Headers) لمنع كاش المانيفست
+  async headers() {
+    return [
+      {
+        source: '/(.*).json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
