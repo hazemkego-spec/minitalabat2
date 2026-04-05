16e3b7c0f5ebe37a19f74934ba02b56034aa2992
hazemkego-spec <hazemkego@gmail.com>
2026-04-05 14:09:05 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -99,7 +99,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content={appTitle} />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1,  viewport-fit=cover" />
       </head>
       <body>
         {children}
